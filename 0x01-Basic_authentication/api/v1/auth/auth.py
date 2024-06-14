8c239677c329c82fad28709f1979ad3ad5f1c5a3
HoodaHashem <mahmoudhashem335@gmail.com>
2024-06-14 20:32:02 +0300

    update auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,19 +3,25 @@
 """Auth module"""
 from flask import request
 from typing import List, TypeVar
+import fnmatch
 
 
-class Auth():
-    """Auth class"""
-
+class Auth:
+    """Authentication class.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Require auth method"""
-        if path is None or excluded_paths is None or excluded_paths == []:
+        """ Method to check if auth is required.
+        """
+        if path is None:
             return True
-        if path[-1] != '/':
-            path += '/'
-        if path in excluded_paths:
-            return False
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
