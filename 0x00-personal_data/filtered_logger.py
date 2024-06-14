622e9e4e15f011eece11c63440cc06b12167517e
HoodaHashem <mahmoudhashem335@gmail.com>
2024-06-14 17:07:01 +0300

    again

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,57 +1,38 @@
 #!/usr/bin/env python3
-
-"""filtered_logger module"""
-import re
+"""
+Module for handling Personal Data
+"""
 from typing import List
+import re
 import logging
-import environ
+from os import environ
 import mysql.connector
 
 
-class RedactingFormatter(logging.Formatter):
-    """
-    Redacting Formatter class
-    """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]) -> None:
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-    def format(self, record: logging.LogRecord) -> str:
-        """ Filters values in incoming log records using filter_datum """
-        record.msg = filter_datum(self.fields, self.REDACTION,
-                                  record.getMessage(), self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
-
-
-PII_FIELDS = ("name", "email", "password", "ssn", "phone")
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
 
 
 def get_logger() -> logging.Logger:
-    """returns a logging.Logger object"""
-
+    """ Returns a Logger Object """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    handler = logging.StreamHandler()
-    formatter = RedactingFormatter(list(PII_FIELDS))
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
     return logger
 
-
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """returns the log message obfuscated"""
-    for field in fields:
-        message = re.sub(rf'{field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Returns a connector to a MySQL database """
@@ -66,7 +47,8 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
                                                      database=db_name)
     return conn
 
-def main() -> None:
+
+def main():
     """
     Obtain a database connection using get_db and retrieves all rows
     in the users table and display each row under a filtered format
@@ -84,6 +66,25 @@ def main() -> None:
 
     cursor.close()
     db.close()
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filters values in incoming log records using filter_datum """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
 
 
 if __name__ == '__main__':
